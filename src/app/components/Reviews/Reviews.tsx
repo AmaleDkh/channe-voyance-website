754389e694c79f13f852a74664c445e575052414
Amale Dkhissi <amdkhissi@gmail.com>
2025-01-17 22:35:55 +0100

    feat: create reviews component

---
+++ src/app/components/Reviews/Reviews.tsx
@@ -0,0 +1,18 @@
+// Style
+import "./Reviews.scss";
+
+function Reviews() {
+  return (
+    <section className="reviews-section">
+      <h3 className="reviews-section__name">Lorem ipsum</h3>
+      <div className="reviews-section__review">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </div>
+    </section>
+  );
+}
+
+export default Reviews;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import Layout from "./components/Layout/Layout";
 import ImageAndTextSection from "./components/ImageAndTextSection/ImageAndTextSection";
 import ThreeBlocksSection from "./components/ThreeBlocksSection/ThreeBlocksSection";
+import Reviews from "./components/Reviews/Reviews";
 
 export default function Home() {
   return (
     <Layout>
       <ImageAndTextSection />
       <ThreeBlocksSection />
+      <Reviews />
     </Layout>
   );
 }
